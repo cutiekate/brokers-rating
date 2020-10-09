8de83aa7e2635a47857f9fb400c90fd3f91ebd71
cutiekate <cutiekate@outlook.com>
2020-10-09 17:17:23 +0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,26 @@
-'use strict';
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+})
